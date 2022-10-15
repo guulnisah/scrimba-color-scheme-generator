9153df683ef7b634a709bcee8587fb76e60719bd
Gulnisa Huseynli <guulnisah@gmail.com>
2022-10-15 23:24:22 +0400

    add event delegation

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-const colorInput = document.querySelector('#color-input')
+const colorSelect = document.querySelector('#color-input')
 const schemeSelect = document.querySelector('#scheme-options')
 const submitBtn = document.querySelector('#submit-button')
 const colorContents = document.querySelectorAll('.color-content')
 const colorCodes = document.querySelectorAll('.color-code')
-const colorBoxes = document.querySelectorAll('.color-box')
+const colorsContainer = document.querySelector('.container__colors')
 
 submitBtn.addEventListener('click', async () => {
     const schemeValue = schemeSelect.options[schemeSelect.selectedIndex].value
-    const colorValue = colorInput.value.slice(1, 7);
+    const colorValue = colorSelect.value.slice(1, 7);
 
     const response = await fetch(`https://www.thecolorapi.com/scheme?hex=${colorValue}&mode=${schemeValue}&count=5`)
     const data = await response.json()
+
     colorContents.forEach((elem, index) => {
         let color = data.colors[index]
         elem.style.backgroundColor = color.hex.value
@@ -21,10 +22,11 @@ submitBtn.addEventListener('click', async () => {
     })
 });
 
-colorBoxes.forEach((elem) => {
-    elem.addEventListener('click', () => {
-        navigator.clipboard.writeText(elem.querySelector('p').innerText).then(() => {
-            console.log('copied')
+colorsContainer.addEventListener('click', (e) => {
+    if (e.target.matches('.color-content')) {
+        const hexCode = e.target.parentNode.querySelector('p').innerText
+        navigator.clipboard.writeText(hexCode).then(() => {
+            alert('Successfully copied the color.')
         })
-    })
+    }
 })
